8abcd0dc85af5cd6a35917738bdae186b88aece5
guillepuntons <guillepuntons@gmail.com>
2010-09-26 20:51:52 +0000

    todo el sistema

--- SistmaDiseño/src/Persistencia/ExpertosPersistencia/Conexion.java
+++ SistmaDiseño/src/Persistencia/ExpertosPersistencia/Conexion.java
@@ -47,8 +47,8 @@ public class Conexion {
         }
         conn=null;
        try{
-            String userName = "root";
-            String password = "root";
+            String userName = "fido";
+            String password = "fido";
             String url = "jdbc:mysql://localhost:3306/disenio";
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection(url, userName, password);
