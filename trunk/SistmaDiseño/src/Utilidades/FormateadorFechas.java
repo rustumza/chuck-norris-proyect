02d2f7a2ea36fc0ce944a5ac8941a9572a02be2f
dnarce <dnarce@37924f85-35e8-ace2-82bd-f8d68c96acaa>
2010-09-26 20:02:10 +0000

    lista ventana consultar ordenes pendientes

--- trunk/SistmaDiseño/src/Utilidades/FormateadorFechas.java
+++ trunk/SistmaDiseño/src/Utilidades/FormateadorFechas.java
@@ -4,7 +4,7 @@
  */
 package Utilidades;
 
-
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,13 +16,13 @@ public class FormateadorFechas {
 
     private static FormateadorFechas instancia;
     private SimpleDateFormat format_dd_MM_yyyy = new SimpleDateFormat("dd/MM/yyyy");
+    private SimpleDateFormat formatMySQLyyyyMMdd = new  SimpleDateFormat("yyyy-MM-dd");
     private SimpleDateFormat format_yyyy_MM_dd = new SimpleDateFormat("yyyy/MM/dd");
-    private SimpleDateFormat StringAFecha = new SimpleDateFormat("dd/MM/yyyy");
 
-
-    public static FormateadorFechas getInstancia(){
-        if(instancia==null)
+    public static FormateadorFechas getInstancia() {
+        if (instancia == null) {
             instancia = new FormateadorFechas();
+        }
         return instancia;
     }
 
@@ -33,20 +33,35 @@ public class FormateadorFechas {
         return format_dd_MM_yyyy;
     }
 
-
     public SimpleDateFormat getFormat_yyyy_MM_dd() {
         return format_yyyy_MM_dd;
     }
 
-    public Date StringAFecha(String pdatestr){
-
-        Date date = new Date();
-        date.setDate(Integer.valueOf(pdatestr.split("-")[2]));
-        date.setMonth(Integer.valueOf(pdatestr.split("-")[1]));
-        date.setYear(Integer.valueOf(pdatestr.split("-")[0]));
-
-        return date;
+    /**
+     * @return the formatMySQLyyyyMMdd
+     */
+    public SimpleDateFormat getFormatMySQLyyyyMMdd() {
+        return formatMySQLyyyyMMdd;
     }
 
+    public Date StringAFecha(String strFecha) throws ParseException {
 
+        Date nuevaFecha = null;
+
+        if (strFecha.split("-").length != 1) {//la fecha ingresada tiene separador "-"
+            if (strFecha.split("-")[0].length() == 4) {//la fecha ingresada tiene formato yyyy-MM-dd
+                nuevaFecha = formatMySQLyyyyMMdd.parse(strFecha);
+            }else{//la fecha ingresada tiene formato dd-MM-yyyy
+                nuevaFecha = format_dd_MM_yyyy.parse(strFecha);
+            }
+        } else if (strFecha.split("/").length !=1) {//la fecha ingresada tiene separador "/"
+            if(strFecha.split("/")[0].length() == 4){//la fecha ingresada tiene formatoo yyyy/MM/dd
+                nuevaFecha = format_yyyy_MM_dd.parse(strFecha);
+            }else{//la fecha ingresada tiene formato dd/MM/yyyy
+                nuevaFecha = format_dd_MM_yyyy.parse(strFecha);
+            }
+        }
+
+        return nuevaFecha;
+    }
 }
